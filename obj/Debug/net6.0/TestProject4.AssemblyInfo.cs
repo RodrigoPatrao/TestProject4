7878f14139fcb2e5563de7a5d686c2a0726c6a2d
Rodrigo Patrão Viveiros <rodrigo.viveiros@outlook.com>
2025-08-08 23:16:17 -0300

    Escolher o tipo de dados correto em seu código C# - concluído

--- obj/Debug/net6.0/TestProject4.AssemblyInfo.cs
+++ obj/Debug/net6.0/TestProject4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e885db11b0d801badc4fc7184c631e712e271f49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2638dc742c444378b00f4795310b3e94b56dae5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
