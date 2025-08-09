2638dc742c444378b00f4795310b3e94b56dae5d
Rodrigo Patrão Viveiros <rodrigo.viveiros@outlook.com>
2025-08-08 22:58:25 -0300

    diferença de tipos de valor e tipos de referência

--- Program.cs
+++ Program.cs
@@ -6,24 +6,36 @@ namespace TestProject4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Signed integral types:");
-            Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-            Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-            Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-            Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+            //ilustrando tipo de referência
+            // int[] data = new int[3];
+            // foreach (var d in data)
+            // {
+            //     Console.WriteLine(data[d]);
+            // }
 
-            Console.WriteLine("");
-            Console.WriteLine("Unsigned integral types:");
-            Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-            Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-            Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-            Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+            //tipo de valor
+            int val_A = 2;
+            int val_B = val_A;
+            // val_B é uma cópia de val_A
+            val_B = 5;
+            // Alterando val_B não afeta val_A, pois são tipos de valor.
 
-            Console.WriteLine("");
-            Console.WriteLine("Floating point types:");
-            Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-            Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-            Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+            Console.WriteLine("--Value Types--");
+            Console.WriteLine($"val_A: {val_A}");
+            Console.WriteLine($"val_B: {val_B}");
+
+            //tipo de referência
+            int[] ref_A = new int[1];
+            ref_A[0] = 2;
+            int[] ref_B = ref_A;
+            // ref_B é uma referência ao mesmo array que ref_A
+            // Alterando o valor em ref_B também altera ref_A, pois ambos referenciam
+            // o mesmo array na memória.
+            ref_B[0] = 5;
+
+            Console.WriteLine("--Reference Types--");
+            Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+            Console.WriteLine($"ref_B[0]: {ref_B[0]}");
         }
     }
 }
